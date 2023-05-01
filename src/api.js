b07212ccaef359df5ea229d8a2b3f5a9947d4d4a
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-01 13:47:18 +0300

    Data Fetching Cleanup

--- src/api.js
+++ src/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-const SearchImages = async () => {
+const SearchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
       Authorization: "Client-ID 6RBlXFTagkZXSFXQp9FWjIDVBeONv43A_x2AnhKL0vA",
     },
     params: {
-      query: "cars",
+      query: term,
     },
   });
 
   console.log(response);
-  return response;
+  return response.data.results;
 };
 
 export default SearchImages;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import SearchImages from './api';
-
-SearchImages();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
