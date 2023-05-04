0503a08c03a663e91c21370ec7d1c69c1d799f7e
Ramazan Mert Kaya <kmert0@outlook.com>
2023-05-04 11:40:32 +0300

    Communicating the List of Images Down

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 import SearchImages from "./api";
 
 function App() {
-  const handleSubmit = (term) => {
-    SearchImages(term);
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (term) => {
+    const result = await SearchImages(term);
+
+    setImages(result);
   };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
